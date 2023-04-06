e6a08a1a6973034e0c082e1758abe80811413eb3
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-04-06 14:44:57 +0100

    added all Dolphin Flat request

---
+++ scrape.mjs
@@ -0,0 +1,32 @@
+import { load } from 'cheerio';
+import fetch from 'node-fetch'; // for use in Node.js
+
+// getData();
+
+async function getDolphinLivingFlats() {
+  try {
+    const url = 'https://www.dolphinliving.com/find-a-home/available-homes';
+    const response = await fetch(url);
+    const body = await response.text();
+    const $ = load(body);
+
+    const allFlats = [];
+
+    $('.views-row').each((i, el) => {
+      const flatInfo =         $(el)
+      .text()
+      .split('\n')
+      .map((str) => str.trim())
+      .filter(Boolean)
+      allFlats.push(flatInfo);
+
+    });
+
+    console.log(allFlats);
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getDolphinLivingFlats();
